a8d9f1d71aacddda0185c9c336f92b46485c0435
aiengage <engageai007@gmail.com>
2024-11-15 23:48:01 +0530

    Update env.js

--- client/dist/env.js
+++ client/dist/env.js
@@ -1,4 +1,4 @@
 // const url = "http://localhost:3000";
-const url = "https://aicalling-demo.onrender.com";
+const url = "https://aienagage.onrender.com";
 
 const callToken = "22576079-730d-4707-b8ab-f780113249f3"
